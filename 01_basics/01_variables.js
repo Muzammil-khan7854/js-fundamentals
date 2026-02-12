216490049b98450a67bf73f11753a1c3b23f5a52
Muzammil khan <muzammilkhan96541658@gmail.com>
2026-02-12 11:26:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const acId=12345;
+let  acName="Muzammil";
+var acEmail="muzammil@gmail.com"; //perfer not to use var because of issue in block scope and functional scope
+acState="Delhi"; // Not use this santax 
+let acCity;
+
+// console.log(acState);
+acName="Sher";
+ acEmail="Sher@gmail.com";
+acState="Mumbai";
+
+
+console.table([acId,acName,acEmail,acState,acCity]);
