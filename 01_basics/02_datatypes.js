2620da3474e5437a260d796b7df4a2147b018256
Muzammil khan <muzammilkhan96541658@gmail.com>
2026-02-12 11:30:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use stict"; //treat all JS code as newer version
+
+//alert(3+3) //we ared use node.js not browser
+
+//datatypes
+
+// Number=> 2 to power 53
+// string => ""
+// boolean => true/false
+// bigint
+// null=> standlone value
+//undefined =>
+//symbol => unique 
+//object
+
+console.log(typeof "Muzammil");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
